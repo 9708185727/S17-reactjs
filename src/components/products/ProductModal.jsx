726a5a8ad6aaac0ce54a7f264dd11d5f525fb389
raman <ramancdry2058@gmail.com>
2025-01-27 15:59:20 +0545

    changes

--- src/components/products/ProductModal.jsx
+++ src/components/products/ProductModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import Title from "../Title";
 import { RxCross1 } from "react-icons/rx";
 const ProductModal = ({isOpen=false,setOpen,label,body,button}) => {
